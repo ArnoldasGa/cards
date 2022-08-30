35288afd68adbd6be34a192c60e23d8929134164
unknown <arnoldasgaigalas@gmail.com>
2022-08-30 18:57:33 +0300

    first commit

---
+++ js/data.js
@@ -0,0 +1,6 @@
+let array1 = [
+    {brand : 'opel', model : 'ascona', color : 'red'}, 
+    {brand : 'ford', model : 'mondeo', color : '#000000'},
+    {brand : 'honda', model : 'civic', color : '#000000'}, 
+    {brand : 'kia', model : 'ceed', color : 'rgb(255,255,0)'}
+]
